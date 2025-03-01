c638b6f323a630429a515d6a2c60e0fe92cdb837
Landon7676 <landonj@umich.edu>
2025-03-01 11:49:42 -0500

    Unisex Bathroom part B

--- unisexBathroomB.cpp
+++ unisexBathroomB.cpp
@@ -8,23 +8,31 @@
 
 using namespace std;
 
-//Part a
-
 class UnisexBathroom {
 private:
     mutex mtx;
-    condition_variable cv;
+    condition_variable cvMen, cvWomen;
     int menInside = 0, womenInside = 0;
+    int menWaiting = 0, womenWaiting = 0;
+    int wCount = 0, mCount = 0;
     int capacity = 3;
+    const int switchNum = 10;
+    bool menTurn = true;  // Ensures fairness by tracking whose turn it is
 
 public:
     void manEnter(int id) {
         unique_lock<mutex> lock(mtx);
+        menWaiting++;
 
-        // Wait if women are in the bathroom or it's full
-        cv.wait(lock, [this]() { return womenInside == 0 && menInside < capacity; });
+        // Wait if women are inside, bathroom is full, or it's not men's turn
+        cvMen.wait(lock, [this]() { 
+            return womenInside == 0 && menInside < capacity 
+                && menTurn && mCount < switchNum; 
+        });
 
+        menWaiting--;
         menInside++;
+        mCount++;
         cout << "Man " << id << " entered. (Men inside: " << menInside << ")\n";
     }
 
@@ -33,17 +41,30 @@ public:
         menInside--;
         cout << "Man " << id << " exited. (Men inside: " << menInside << ")\n";
 
-        // Notify waiting threads
-        cv.notify_all();
+        if (menInside == 0 && (mCount >= switchNum || womenWaiting > 0)) {
+            // Force switch to women
+            mCount = 0;
+            menTurn = false;
+            cvWomen.notify_all();
+        } else {
+            cvMen.notify_one();
+        }
     }
 
     void womanEnter(int id) {
         unique_lock<mutex> lock(mtx);
+        womenWaiting++;
 
-        // Wait if men are in the bathroom or it's full
-        cv.wait(lock, [this]() { return menInside == 0 && womenInside < capacity; });
+        // Wait if men are inside, bathroom is full, or it's not women's turn
+        cvWomen.wait(lock, [this]() { 
+            return menInside == 0 && womenInside < capacity 
+                && !menTurn && wCount < switchNum; 
+        });
 
+        womenWaiting--;
         womenInside++;
+        wCount++;
+
         cout << "Woman " << id << " entered. (Women inside: " << womenInside << ")\n";
     }
 
@@ -52,8 +73,14 @@ public:
         womenInside--;
         cout << "Woman " << id << " exited. (Women inside: " << womenInside << ")\n";
 
-        // Notify waiting threads
-        cv.notify_all();
+        if (womenInside == 0 && (wCount >= switchNum || menWaiting > 0)) {
+            // Force switch to men
+            wCount = 0;
+            menTurn = true;
+            cvMen.notify_all();
+        } else {
+            cvWomen.notify_one();
+        }
     }
 };
 
@@ -85,7 +112,7 @@ int main() {
     vector<thread> people;
 
     // Create 10 men and 10 women threads
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         people.push_back(thread(OneMan, ref(bathroom), i + 1));
         people.push_back(thread(OneWoman, ref(bathroom), i + 1));
     }
